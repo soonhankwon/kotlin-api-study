8bc4df0a9ced6997f356e65c9af6a0a1993e0e45
soonhankwon <soonable@gmail.com>
2023-10-17 19:30:58 +0900

    init

---
+++ src/main/kotlin/dev/soon/kotlinapistudy/KotlinApiStudyApplication.kt
@@ -0,0 +1,11 @@
+package dev.soon.kotlinapistudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApiStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinApiStudyApplication>(*args)
+}
---
+++ src/test/kotlin/dev/soon/kotlinapistudy/KotlinApiStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.soon.kotlinapistudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApiStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
