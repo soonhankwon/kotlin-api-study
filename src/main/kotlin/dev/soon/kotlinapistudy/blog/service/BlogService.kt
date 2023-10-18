5c642a1fdd1f981f6659e0c7e40606967b2fa4fa
soonhankwon <soonable@gmail.com>
2023-10-18 17:42:00 +0900

    feat : 검색어 저장 추가 및 검색어 랭킹 조회 메서드

--- src/main/kotlin/dev/soon/kotlinapistudy/blog/service/BlogService.kt
+++ src/main/kotlin/dev/soon/kotlinapistudy/blog/service/BlogService.kt
@@ -1,19 +1,25 @@
 package dev.soon.kotlinapistudy.blog.service
 
 import dev.soon.kotlinapistudy.blog.dto.BlogDto
+import dev.soon.kotlinapistudy.blog.entity.WordCount
+import dev.soon.kotlinapistudy.blog.repository.WordCountRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
 @Service
-class BlogService{
+class BlogService(
+    val wordCountRepository : WordCountRepository
+){
 
     @Value("\${kakao.rest-api.key}")
     lateinit var kakaoRestApiKey: String
 
+    @Transactional
     fun searchKakao(blogDto: BlogDto): String? {
         val webClient = WebClient
             .builder()
@@ -35,6 +41,15 @@ class BlogService{
             .retrieve()
             .bodyToMono<String>()
 
+        val lowQuery: String = blogDto.query.lowercase()
+        val word: WordCount = wordCountRepository.findById(lowQuery)
+            .orElse(WordCount(lowQuery))
+        word.cnt++
+
+        wordCountRepository.save(word)
+
         return response.block()
     }
+
+    fun searchWordRank(): List<WordCount> = wordCountRepository.findTop10ByOrderByCntDesc()
 }
