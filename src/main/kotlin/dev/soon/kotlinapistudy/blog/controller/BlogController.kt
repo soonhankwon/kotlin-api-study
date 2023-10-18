3069e955df8f6d627827dde82c022c931c2cce50
soonhankwon <soonable@gmail.com>
2023-10-18 14:30:18 +0900

    feat : BlogController

---
+++ src/main/kotlin/dev/soon/kotlinapistudy/blog/controller/BlogController.kt
@@ -0,0 +1,19 @@
+package dev.soon.kotlinapistudy.blog.controller
+
+import dev.soon.kotlinapistudy.blog.dto.BlogDto
+import dev.soon.kotlinapistudy.blog.service.BlogService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/blog")
+@RestController
+class BlogController(
+    val blogService: BlogService
+) {
+    @GetMapping
+    fun searchBlog(@RequestBody blogDto: BlogDto): String? {
+        return blogService.searchKakao(blogDto)
+    }
+}
