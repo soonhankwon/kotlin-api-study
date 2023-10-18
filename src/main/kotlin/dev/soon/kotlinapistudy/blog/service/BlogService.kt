cadb76911ca1f9f3c373d8b77867da5cad41514e
soonhankwon <soonable@gmail.com>
2023-10-18 15:06:01 +0900

    feat : kakao open api 적용 (webclient 사용)

--- src/main/kotlin/dev/soon/kotlinapistudy/blog/service/BlogService.kt
+++ src/main/kotlin/dev/soon/kotlinapistudy/blog/service/BlogService.kt
@@ -1,13 +1,40 @@
 package dev.soon.kotlinapistudy.blog.service
 
 import dev.soon.kotlinapistudy.blog.dto.BlogDto
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @Service
-class BlogService {
+class BlogService{
+
+    @Value("\${kakao.rest-api.key}")
+    lateinit var kakaoRestApiKey: String
 
     fun searchKakao(blogDto: BlogDto): String? {
-        println(blogDto.toString())
-        return "SearchKakao"
+        val webClient = WebClient
+            .builder()
+            .baseUrl("https://dapi.kakao.com")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build()
+
+        val response = webClient
+            .get()
+            .uri {
+                it.path("/v2/search/blog")
+                    .queryParam("query", blogDto.query)
+                    .queryParam("sort", blogDto.sort)
+                    .queryParam("page", blogDto.page)
+                    .queryParam("size", blogDto.size)
+                    .build()
+            }
+            .header("Authorization", "KakaoAK $kakaoRestApiKey")
+            .retrieve()
+            .bodyToMono<String>()
+
+        return response.block()
     }
 }
