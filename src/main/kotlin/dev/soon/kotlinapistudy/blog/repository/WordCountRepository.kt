db9f0bb6d320b80f9cb7a251cf9c767b6c5d7e57
soonhankwon <soonable@gmail.com>
2023-10-18 17:42:52 +0900

    feat : 검색어 랭킹 탑10 조회 메서드

--- src/main/kotlin/dev/soon/kotlinapistudy/blog/repository/WordCountRepository.kt
+++ src/main/kotlin/dev/soon/kotlinapistudy/blog/repository/WordCountRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface WordCountRepository : JpaRepository<WordCount, String>
+interface WordCountRepository : JpaRepository<WordCount, String> {
+    fun findTop10ByOrderByCntDesc(): List<WordCount>
+}
